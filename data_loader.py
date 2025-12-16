9eeffbdb4fe7896c798e0ad0883428487808ab92
ahmetax <ahmetax@gmail.com>
2025-12-16 16:28:45 +0300

    kelimeler tablosu

--- data_loader.py
+++ data_loader.py
@@ -107,6 +107,9 @@ class DBManager:
                 aciklama TEXT,
                 onay INTEGER DEFAULT 0,
                 hata INTEGER DEFAULT 0,
+                tip TEXT,
+                detay TEXT,
+                skor INTEGER DEFAULT 0,
                 CHECK (LENGTH(kelime) > 0)
             );
         """
